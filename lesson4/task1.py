b99ef72d283d66635a69adf447cfc746cbbf9e97
doniyor4675-coder <doniyor4675@gmail.com>
2025-08-12 23:41:14 +0700

    dars

---
+++ lesson4/task1.py
@@ -0,0 +1,12 @@
+# #task_1
+# a, b = 5, 5
+# print('Natija quyidagicha: ', a + b)
+
+#task_2
+def addinner(x, y):
+    return x + y
+print(addinner(5, 3))
+
+#bu oddiyrog'i
+x, y = 5, 3
+print(x + y)
