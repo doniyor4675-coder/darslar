bc89dc7a9fa46ba2b23a3685f728ba5ec76054f8
doniyor4675-coder <doniyor4675@gmail.com>
2025-08-13 01:04:04 +0700

    yakuniy fayl

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -2,11 +2,26 @@
 # a, b = 5, 5
 # print('Natija quyidagicha: ', a + b)
 
-#task_2
-def addinner(x, y):
-    return x + y
-print(addinner(5, 3))
+# #task_2
+# def addinner(x, y):
+#     return x + y
+# print(addinner(5, 3))
 
-#bu oddiyrog'i
-x, y = 5, 3
-print(x + y)
+# #bu oddiyrog'i
+# x, y = 5, 3
+# print(x + y)
+
+# #task_3
+# x, y = 1200, 30 #berilgan sonlar
+# print(x / y) #bu yerda 1200/30 ning qiymatini ekranga chiqaryapti
+
+#task_4
+result = sum([3, 5, 7])
+print(result)
+
+a, b, c = 3, 5, 7
+result = a + b + c
+print(result)
+
+#chatgptni aytishicha birinchi variant yaxshiroq ekan.
+#Chunki birinchisi universal va ishlashga qulay ekan.
