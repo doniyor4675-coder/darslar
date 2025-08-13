16834e30ad3c351888be67dc76315c02ea83caa1
doniyor4675-coder <doniyor4675@gmail.com>
2025-08-14 00:35:10 +0700

    beshinchi darsda o'tilgan narsalar ro'yhatlar bilan ishlash

---
+++ lesson5/Untitled-1.py
@@ -0,0 +1,43 @@
+#students_list = ['men', 'sen', 'u', 1, 2, 3.4, True, False]
+#print(students_list)
+
+# student = students_list[0].title()
+# print(student)
+
+# student = students_list[0].upper()
+# print(student)
+
+# student = students_list[0].lower()
+# print(student)
+
+# student = students_list[0].removesuffix('n')
+# print(student)
+
+# student = students_list[0].removeprefix('m')
+# print(student)
+
+# student = students_list[-8]
+# print(student)
+
+# for i in range(8):
+#     print('Salom, ', students_list[i])
+
+# students_list[0] = 'O\'zim'
+# print(students_list)
+
+# students_list.append('nimadir')
+# print(students_list)
+
+# students_list.remove('nimadir')
+# print(students_list)
+
+# students_list.pop(7)
+# print(students_list)
+
+#mevalar ro'yhatini tuzdim
+mevalar = ['olma', 'olcha', 'shaftoli']
+print(mevalar) #uni ekranga chiqardim
+mevalar[1] = 'olma' #keyin olmani qo'shdim
+print(mevalar) #yana ekranga chiqardim
+mevalar.append('nok') #keyin nok qo'shish esga keldi
+print(mevalar) #oxiriga yakuniy ro'yhatni chiqardim
