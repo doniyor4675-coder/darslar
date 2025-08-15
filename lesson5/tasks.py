137f95b99da52670db79d3aa5c00978c0fd0c38c
doniyor4675-coder <doniyor4675@gmail.com>
2025-08-15 22:57:21 +0700

    yakuniy tayyor uyga vazifa

--- lesson5/tasks.py
+++ lesson5/tasks.py
@@ -1,44 +1,44 @@
-# #task_3-1
-# print('mashq 3-1')
-# names = ['sardor', 'sarvar', 'sanjar']
-# for i in range(len(names)):
-#     print(names[i].title())
+#task_3-1
+print('mashq 3-1')
+names = ['sardor', 'sarvar', 'sanjar']
+for i in range(len(names)):
+    print(names[i].title())
 
-# #task_3-2
-# print('mashq 3-2')
-# names = ['sardor', 'sarvar', 'sanjar']
-# for i in range(len(names)):
-#     print('Salom,', names[i].title())
+#task_3-2
+print('mashq 3-2')
+names = ['sardor', 'sarvar', 'sanjar']
+for i in range(len(names)):
+    print('Salom,', names[i].title())
 
-# #task_3-3
-# print('mashq 3-3')
-# names = ['tiko', 'matiz', 'spark']
-# for i in range(len(names)):
-#     print('Men', names[i].title(), 'sotib olmoqchiman.')
+#task_3-3
+print('mashq 3-3')
+names = ['tiko', 'matiz', 'spark']
+for i in range(len(names)):
+    print('Men', names[i].title(), 'sotib olmoqchiman.')
 
-# #task_3-4
-# print('mashq 3-4')
-# names = ['sarvinoz', 'sevara', 'setora']
-# for i in range(len(names)):
-#     print('Salom, ', names[i].title(), '! Men bugun sizni tushlikka taklif qilmoqchiman.', sep='')
+#task_3-4
+print('mashq 3-4')
+names = ['sarvinoz', 'sevara', 'setora']
+for i in range(len(names)):
+    print('Salom, ', names[i].title(), '! Men bugun sizni tushlikka taklif qilmoqchiman.', sep='')
 
-# #task_3-5
-# print('mashq 3-5')
-# fruits = ['olma', 'olcha', 'anor']
-# for i in range(len(fruits)):
-#     print(fruits[i].title())
+#task_3-5
+print('mashq 3-5')
+fruits = ['olma', 'olcha', 'anor']
+for i in range(len(fruits)):
+    print(fruits[i].title())
 
-# #task_3-6
-# print('mashq 3-6')
-# fruits = ['olma', 'olcha', 'anor']
-# for i in range(len(fruits)):
-#     print(f'Men {fruits[i]}ni juda yaxshi ko\'raman.')
+#task_3-6
+print('mashq 3-6')
+fruits = ['olma', 'olcha', 'anor']
+for i in range(len(fruits)):
+    print(f'Men {fruits[i]}ni juda yaxshi ko\'raman.')
 
-# #task_3-7
-# print('mashq 3-7')
-# cities = ['andijon', 'namangan', 'farg\'ona']
-# for i in range(len(cities)):
-#     print(f'Men {cities[i].title()} shahriga sayohat qilishni xohlayman.')
+#task_3-7
+print('mashq 3-7')
+cities = ['andijon', 'namangan', 'farg\'ona']
+for i in range(len(cities)):
+    print(f'Men {cities[i].title()} shahriga sayohat qilishni xohlayman.')
 
 #task_3-8
 print('mashq 3-8')
