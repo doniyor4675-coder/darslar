fe6cb7351ddf92bbf24b4213ef4d256fb9c63721
doniyor4675-coder <doniyor4675@gmail.com>
2025-08-26 00:41:10 +0700

    davomi

---
+++ lesson9/lesson9.py
@@ -0,0 +1,44 @@
+# raqam_1 = int(input('BIrinchi raqam: '))
+# raqam_2 = int(input('Ikkinchi raqam: '))
+# amal = input('Amalni kiriting (+, -, *, /, **): ')
+# amallar = ['+', '-', '*', '/', '**']
+# if amal in amallar:
+#     if amal == '+':
+#         print(raqam_1 + raqam_2)
+#     elif amal == '-':
+#         print(raqam_1 - raqam_2)
+#     elif amal == '*':
+#         print(raqam_1 * raqam_2)
+#     elif amal == '/':
+#         print(raqam_1 / raqam_2)
+#     elif amal == '**':
+#         print(raqam_1 ** raqam_2)
+# else:
+#     print('Bunday amal yo\'q!')
+
+# mevalar = ['olma', 'nok', 'uzum']
+# meva = 'shaftoli'
+# if meva in mevalar:
+#     print('Meva mevalar ichida bor.')
+# else:
+#     print('Bunday meva ro\'yhatda yo\'q.')
+
+# royhat = ['1']
+# if royhat:    #royhatni bosh yoki boshmasligini aniqlash
+#     print('false')
+
+# mevalar = ['olma', 'nok', 'uzum']
+# for meva in mevalar:
+#     if meva == 'nok':
+#         print('Bunday meva bor.')
+#     else:
+#         print('Bunday meva yo\'q')
+
+# tolov_qilgan_talabalar = ['asrorbek', 'doniyor', 'ali']
+# darsga_qoyiladigan_talabalar = []
+# ism = input('Ismingizni kiriting: ')
+# if ism in tolov_qilgan_talabalar:
+#     darsga_qoyiladigan_talabalar.append(ism)
+# else:
+#     print('Siz avval tolov qilishingiz kerak!')
+# print(darsga_qoyiladigan_talabalar)
